4a3bed0ba6d4c21491e4a889239532f5c4a0c3f7
Victor Yamchinov <kodiers@gmail.com>
2018-11-28 02:45:07 +0300

    completed contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import {NgModule} from '@angular/core';
 import {HomeComponent} from './core/home/home.component';
 import {NotFoundComponent} from './core/not-found/not-found.component';
 import {AboutComponent} from './core/about/about.component';
+import {ContactComponent} from './core/contact/contact.component';
 
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent},
+  { path: 'contact', component: ContactComponent},
   { path: '**', component: NotFoundComponent }
 ];
 
