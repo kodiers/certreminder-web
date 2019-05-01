77dc33b3fa67c1171c1648d09ed2207d56abd6b5
Victor Yamchinov <kodiers@gmail.com>
2019-05-01 05:01:45 +0300

    start fix breaking changes

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,54 +1,51 @@
-
-import {switchMap, tap, map, mergeMap} from 'rxjs/operators';
 import {Injectable} from '@angular/core';
 import {Router} from '@angular/router';
-
-
-
-
-
+import {switchMap, tap, map, mergeMap} from 'rxjs/operators';
 import {Observable} from 'rxjs';
-
 import {Actions, Effect, ofType} from '@ngrx/effects';
 
 import * as fromAuthActions from './auth.actions';
 import {AuthService} from '../auth.service';
-import {Profile} from '../../shared/models/profile.model';
+
 
 @Injectable()
 export class AuthEffects {
   constructor(private actions$: Actions, private router: Router, private authSvc: AuthService) {}
 
-  @Effect() authSignup = this.actions$.ofType(fromAuthActions.TRY_SIGNUP).pipe(map((action: fromAuthActions.TrySignup) => {
-    return action.payload;
-  }),switchMap(data => {
-    return this.authSvc.registerUser(data.username, data.password, data.confirm_password);
-  }),mergeMap(data => {
-    if (data.error === null) {
-      const user = data.user;
-      this.authSvc.setToken(user.token);
-      this.router.navigate(['/']);
+  @Effect() authSignup = this.actions$.pipe(
+    ofType(fromAuthActions.TRY_SIGNUP),
+    map((action: fromAuthActions.TrySignup) => {
+      return action.payload;
+    }),
+    switchMap(data => {
+      return this.authSvc.registerUser(data.username, data.password, data.confirm_password);
+    }),
+    mergeMap(data => {
+      if (data.error === null) {
+        const user = data.user;
+        this.authSvc.setToken(user.token);
+        this.router.navigate(['/']);
+        return [
+          {
+            type: fromAuthActions.SIGNUP
+          },
+          {
+            type: fromAuthActions.SET_TOKEN,
+            payload: user.token
+          },
+          {
+            type: fromAuthActions.GET_PROFILE,
+            payload: user
+          },
+          {
+            type: fromAuthActions.SIGNIN
+          }
+        ];
+      }
       return [
-        {
-          type: fromAuthActions.SIGNUP
-        },
-        {
-          type: fromAuthActions.SET_TOKEN,
-          payload: user.token
-        },
-        {
-          type: fromAuthActions.GET_PROFILE,
-          payload: user
-        },
-        {
-          type: fromAuthActions.SIGNIN
-        }
+        {type: fromAuthActions.SIGNUP_FAILURE}
       ];
-    }
-    return [
-      {type: fromAuthActions.SIGNUP_FAILURE}
-    ];
-  }),);
+    }));
 
   @Effect({dispatch: false}) authLogout = this.actions$.ofType(fromAuthActions.LOGOUT).pipe(tap(() => {
     this.authSvc.removeToken();
