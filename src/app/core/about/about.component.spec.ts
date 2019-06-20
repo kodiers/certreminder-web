537a11873d5d0b435725deae845634683a768b9d
Victor Yamchinov <kodiers@gmail.com>
2019-06-21 00:53:10 +0300

    added about tests

--- src/app/core/about/about.component.spec.ts
+++ src/app/core/about/about.component.spec.ts
@@ -31,4 +31,15 @@ describe('AboutComponent', () => {
       ' This application is designed for IT professionals who are forced to track multiple certifications. You can create a certification (or add your own if it is not in our database), specify the date of passing and add examinations to it (or add your own if not in our database) and indicate the date of their passing. Hope this application helps you keep up. '
     );
   });
+
+  it('should have terms of use', () => {
+    let element = fixture.debugElement.nativeElement;
+    expect(element.querySelector('h2').textContent).toEqual('Terms of Use');
+  });
+
+  it('should have list', () => {
+    let element = fixture.debugElement.nativeElement;
+    console.log(element.querySelector('li'));
+    expect(element.querySelector('li').textContent).toContain('Parties This Agreement is between you and Re:Minder only. Re:Minder, is solely responsible for the Re:Minder App and its content.')
+  });
 });
