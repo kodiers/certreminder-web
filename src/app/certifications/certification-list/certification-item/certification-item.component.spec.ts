1249891314f9717dca483780991a87556343ab8d
Victor Yamchinov <kodiers@gmail.com>
2019-07-18 01:45:33 +0300

    completed tests for certification-item.component

--- src/app/certifications/certification-list/certification-item/certification-item.component.spec.ts
+++ src/app/certifications/certification-list/certification-item/certification-item.component.spec.ts
@@ -1,6 +1,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 import { CertificationItemComponent } from './certification-item.component';
+import {SharedModule} from '../../../shared/shared.module';
+import {Vendor} from '../../../shared/models/vendor.model';
+import {Component, Input} from '@angular/core';
+import {Certification} from '../../../shared/models/certification.model';
+import {By} from '@angular/platform-browser';
+
+
+@Component({selector: 'app-certification-exam-list', template: ''})
+class CertificationExamListStub {
+  @Input() certification;
+  @Input() vendor;
+}
+
+
+const vendor = new Vendor(1, new Date(), new Date(), 'test', 'test.jpg', 'test');
+const certification = new Certification(1, new Date(), new Date(), 'test', 'test', 'test.jpg',
+  'test', false, vendor.id);
+
 
 describe('CertificationItemComponent', () => {
   let component: CertificationItemComponent;
@@ -8,7 +27,14 @@ describe('CertificationItemComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CertificationItemComponent ]
+      declarations: [
+        CertificationItemComponent,
+        CertificationExamListStub
+      ],
+      imports: [
+        SharedModule,
+        HttpClientTestingModule,
+      ]
     })
     .compileComponents();
   }));
@@ -16,10 +42,26 @@ describe('CertificationItemComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CertificationItemComponent);
     component = fixture.componentInstance;
+    component.certification = certification;
+    component.vendors = [vendor];
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display vendor and certification titles', () => {
+    const element = fixture.debugElement;
+    expect(
+      element.query(
+        By.css('span.vendor__title--span')
+      ).nativeElement.textContent
+    ).toEqual(vendor.title);
+    expect(
+      element.query(
+        By.css('h3')
+      ).nativeElement.textContent
+    ).toEqual(vendor.title);
   });
 });
