d09de8276a35c4c47d56329aab249a42523cccb1
Victor Yamchinov <kodiers@gmail.com>
2019-08-08 02:30:50 +0300

    started tests for new-user-certification.component

--- src/app/user-certifications/new-user-certification/new-user-certification.component.spec.ts
+++ src/app/user-certifications/new-user-certification/new-user-certification.component.spec.ts
@@ -1,16 +1,52 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NewUserCertificationComponent } from './new-user-certification.component';
+import {SharedModule} from '../../shared/shared.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {NgSelectModule} from '@ng-select/ng-select';
+import {RouterTestingModule} from '@angular/router/testing';
+import {MockStore, provideMockStore} from '@ngrx/store/testing';
+import {Vendor} from '../../shared/models/vendor.model';
+import {vendor} from '../../shared/tests/fixtures/test-data';
+import {Router} from '@angular/router';
+import * as appReducers from '../../store/app.reducers';
+import * as userCertReducers from '../store/user-certifications.reducers';
+import {Store, StoreModule} from '@ngrx/store';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+
+const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
 
 describe('NewUserCertificationComponent', () => {
   let component: NewUserCertificationComponent;
   let fixture: ComponentFixture<NewUserCertificationComponent>;
+  let store: MockStore<{userCerts: {vendors: Vendor[]}}>;
+  let initialState = {userCerts: {vendors: [vendor]}};
+  let router: Router;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NewUserCertificationComponent ]
+      declarations: [ NewUserCertificationComponent ],
+      imports: [
+        SharedModule,
+        FormsModule,
+        NgSelectModule,
+        ReactiveFormsModule,
+        RouterTestingModule,
+        StoreModule.forRoot({
+          ...appReducers.reducers,
+          feature: userCertReducers.userCertReducer
+        }),
+        HttpClientTestingModule
+      ],
+      providers: [
+        provideMockStore({initialState}),
+        {provide: Router, useValue: routerSpy}
+      ]
     })
     .compileComponents();
+    store = TestBed.get(Store);
+    router = TestBed.get(Router);
+    spyOn(store, 'dispatch').and.callThrough();
   }));
 
   beforeEach(() => {
