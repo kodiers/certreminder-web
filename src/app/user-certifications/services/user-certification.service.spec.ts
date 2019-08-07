e085ce5726806f38eb84ca86ce26ca5ebaf11262
Victor Yamchinov <kodiers@gmail.com>
2019-08-08 02:00:46 +0300

    completed tests for user-certification.service

--- src/app/user-certifications/services/user-certification.service.spec.ts
+++ src/app/user-certifications/services/user-certification.service.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed, inject } from '@angular/core/testing';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 
 import { UserCertificationService } from './user-certification.service';
-import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 import {userCertification} from '../../shared/tests/fixtures/test-data';
 import {API_URL} from '../../shared/constants';
+import {formatDateToStr} from '../../shared/helpers/functions';
+
 
 describe('UserCertificationService', () => {
   let httpTestingController: HttpTestingController;
@@ -53,6 +55,34 @@ describe('UserCertificationService', () => {
       expect(data).toEqual(userCertification);
     });
     const req = httpTestingController.expectOne(API_URL + `remainder/certification/${userCertification.id}/`);
+    req.flush(mockResponse);
+  });
+
+  it('should update user certification', () => {
+    const mockResponse = userCertification;
+    userCertificationService.updateUserCertification(userCertification).subscribe(data => {
+      expect(data).toEqual(userCertification);
+    });
+    const req = httpTestingController.expectOne(API_URL + `remainder/certification/${userCertification.id}/`);
+    req.flush(mockResponse);
+  });
+
+  it('should delete user certification', () => {
+    const mockResponse = null;
+    userCertificationService.deleteUserCertification(userCertification).subscribe(data => {
+      expect(data).toBeNull();
+    });
+    const req = httpTestingController.expectOne(API_URL + `remainder/certification/${userCertification.id}/`);
+    req.flush(mockResponse);
+  });
+
+  it('should cerete user certification', () => {
+    const mockResponse = userCertification;
+    userCertificationService.createUserCertification(userCertification.certification_id, formatDateToStr(userCertification.expiration_date))
+      .subscribe(data => {
+        expect(data).toEqual(userCertification);
+    });
+    const req = httpTestingController.expectOne(API_URL + 'remainder/certification/');
     req.flush(mockResponse);
   });
 });
