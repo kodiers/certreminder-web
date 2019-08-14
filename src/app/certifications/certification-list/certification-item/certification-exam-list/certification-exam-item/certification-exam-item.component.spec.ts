fa37769745b2f3f7b4e9a9073c09e011dd256a96
Victor Yamchinov <kodiers@gmail.com>
2019-08-15 02:38:24 +0300

    fixed certification-exam-item.component test

--- src/app/certifications/certification-list/certification-item/certification-exam-list/certification-exam-item/certification-exam-item.component.spec.ts
+++ src/app/certifications/certification-list/certification-item/certification-exam-list/certification-exam-item/certification-exam-item.component.spec.ts
@@ -2,11 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import {By} from '@angular/platform-browser';
 
 import { CertificationExamItemComponent } from './certification-exam-item.component';
-import {Exam} from '../../../../../shared/models/exam.model';
+import {exam} from '../../../../../shared/tests/fixtures/test-data';
 
-
-const exam = new Exam(1, new Date(), new Date(), 'test', 'test-number',  'test',
-  false, [1]);
 
 describe('CertificationExamItemComponent', () => {
   let component: CertificationExamItemComponent;
@@ -22,8 +19,6 @@ describe('CertificationExamItemComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CertificationExamItemComponent);
     component = fixture.componentInstance;
-    component.exam = exam;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
@@ -31,12 +26,16 @@ describe('CertificationExamItemComponent', () => {
   });
 
   it('should display exam title and number', () => {
+    component.exam = exam;
+    fixture.detectChanges();
     const element = fixture.debugElement;
     expect(element.nativeElement.querySelector('h5').textContent).toContain('test');
     expect(element.query(By.css('div.row > div.col-sm-12 > h5 > span'))).toBeTruthy();
   });
 
   it('should display title only', () => {
+    component.exam = exam;
+    fixture.detectChanges();
     component.exam.number = null;
     fixture.detectChanges();
     const element = fixture.debugElement;
