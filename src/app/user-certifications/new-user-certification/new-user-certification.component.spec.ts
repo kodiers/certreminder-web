88cbcc6411319f7bd440f4a2808e879a4aa6fcd9
Victor Yamchinov <kodiers@gmail.com>
2019-08-15 02:11:04 +0300

    completed tests for new-user-certification.component

--- src/app/user-certifications/new-user-certification/new-user-certification.component.spec.ts
+++ src/app/user-certifications/new-user-certification/new-user-certification.component.spec.ts
@@ -1,27 +1,30 @@
-import {async, ComponentFixture, TestBed, tick} from '@angular/core/testing';
-
-import { NewUserCertificationComponent } from './new-user-certification.component';
-import {SharedModule} from '../../shared/shared.module';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {FormArray, FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {NgSelectModule} from '@ng-select/ng-select';
 import {RouterTestingModule} from '@angular/router/testing';
-import {MockStore, provideMockStore} from '@ngrx/store/testing';
-import {Vendor} from '../../shared/models/vendor.model';
-import {certification, exam, vendor} from '../../shared/tests/fixtures/test-data';
 import {Router} from '@angular/router';
-import * as appReducers from '../../store/app.reducers';
-import * as userCertReducers from '../store/user-certifications.reducers';
-import {Store, StoreModule} from '@ngrx/store';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {of} from 'rxjs';
 import {By} from '@angular/platform-browser';
+
+import {NgSelectModule} from '@ng-select/ng-select';
+import {MockStore, provideMockStore} from '@ngrx/store/testing';
+import {Store, StoreModule} from '@ngrx/store';
+import {NgbModal, NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+
+import { NewUserCertificationComponent } from './new-user-certification.component';
+import {Vendor} from '../../shared/models/vendor.model';
+import {certification, exam, userCertification, userExam, vendor} from '../../shared/tests/fixtures/test-data';
+import * as appReducers from '../../store/app.reducers';
+import * as userCertReducers from '../store/user-certifications.reducers';
 import {CertificationService} from '../../certifications/services/certification.service';
 import {Certification} from '../../shared/models/certification.model';
 import {ExamService} from '../../certifications/services/exam.service';
 import {DateModalComponent} from '../../shared/views/date-modal/date-modal.component';
-import {NgbModal, NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {TitleComponent} from '../../shared/views/title/title.component';
+import {UserCertificationService} from '../services/user-certification.service';
+import {UserExamService} from '../services/user-exam.service';
+import {formatDateToStr} from '../../shared/helpers/functions';
 
 
 const certificationServiceMock = {
@@ -33,6 +36,18 @@ const certificationServiceMock = {
 const examServiceMock = {
   getExamsForCertification: (certification: Certification) => {
     return of([exam]);
+  }
+};
+
+const userCertServiceMock = {
+  createUserCertification: (certificationId: number, expirationDate: string) => {
+    return of(userCertification);
+  }
+};
+
+const userExamServiceMock = {
+  createUsersExams: (userCertId: number, data: [{exam_id: number, date_of_pass: string}]) => {
+    return of([userExam]);
   }
 };
 
@@ -53,6 +68,7 @@ const NgbModalStub = {
 
 const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
 
+
 describe('NewUserCertificationComponent', () => {
   let component: NewUserCertificationComponent;
   let fixture: ComponentFixture<NewUserCertificationComponent>;
@@ -62,6 +78,8 @@ describe('NewUserCertificationComponent', () => {
   let certSvc: CertificationService;
   let examSvc: ExamService;
   let modal: NgbModal;
+  let userCertSvc: UserCertificationService;
+  let userExamSvc: UserExamService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -88,7 +106,9 @@ describe('NewUserCertificationComponent', () => {
         {provide: Router, useValue: routerSpy},
         {provide: CertificationService, useValue: certificationServiceMock},
         {provide: ExamService, useValue: examServiceMock},
-        {provide: NgbModal, useValue: NgbModalStub}
+        {provide: NgbModal, useValue: NgbModalStub},
+        {provide: UserCertificationService, useValue: userCertServiceMock},
+        {provide: UserExamService, useValue: userExamServiceMock}
       ]
     })
     .compileComponents();
@@ -96,6 +116,8 @@ describe('NewUserCertificationComponent', () => {
     router = TestBed.get(Router);
     certSvc = TestBed.get(CertificationService);
     examSvc = TestBed.get(ExamService);
+    userCertSvc = TestBed.get(UserCertificationService);
+    userExamSvc = TestBed.get(UserExamService);
     modal = TestBed.get(NgbModal);
     spyOn(store, 'dispatch').and.callThrough();
   }));
@@ -176,5 +198,48 @@ describe('NewUserCertificationComponent', () => {
     component.chooseExamDate(0);
     fixture.detectChanges();
     expect(component.modalRef).toBeTruthy();
+  });
+
+  it('should get exam date', () => {
+    const date = new Date();
+    component.examDates = [date];
+    const result = component.getExamDate(0);
+    expect(result).toEqual(date);
+  });
+
+  it('should not get exam date', () => {
+    const date = new Date();
+    component.examDates = [date];
+    const result = component.getExamDate(component.examDates.length + 1);
+    expect(result).toBeNull();
+  });
+
+  it('should get controls', () => {
+    component.certifications = [certification];
+    component.addExam();
+    const controls = component.getControls();
+    expect(controls.length).toEqual(1);
+  });
+
+  it('should save data', () => {
+    const date = new Date();
+    component.certifications = [certification];
+    component.userCertForm.controls['certification_id'].setValue(certification.id);
+    component.userCertForm.controls['expiration_date'].setValue(formatDateToStr(date));
+    component.addExam();
+    const examControl = (<FormArray>component.userCertForm.controls['exams']).at(0);
+    (<FormArray>component.userCertForm.controls['exams']).at(0)['controls'].date_of_pass.setValue(formatDateToStr(date));
+    (<FormArray>component.userCertForm.controls['exams']).at(0)['controls'].exam_id.setValue(exam.id);
+    component.saveData();
+    const spy = router.navigate as jasmine.Spy;
+    const args = spy.calls.first().args;
+    expect(args[0][0]).toEqual('/user-certifications');
+  });
+
+  it('should navigate', () => {
+    component.navigateToUserCertifications();
+    const spy = router.navigate as jasmine.Spy;
+    const args = spy.calls.first().args;
+    expect(args[0][0]).toEqual('/user-certifications');
   });
 });
